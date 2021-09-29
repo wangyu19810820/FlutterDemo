4fb672f2952a374382cd582bcb88db361000af23
wangyu19810820 <gadfly0036_cn@sina.com>
2021-09-29 19:45:41 +0800

    aa

--- Flutter从入门到进阶实战携程网App/flutter_dart_learn/lib/data_type.dart
+++ Flutter从入门到进阶实战携程网App/flutter_dart_learn/lib/data_type.dart
@@ -10,7 +10,11 @@ class DataType extends StatefulWidget {
 class _DataTypeState extends State<DataType> {
   @override
   Widget build(BuildContext context) {
-    _numType();
+    // _numType();
+    // _stringType();
+    // _boolType();
+    // _listType();
+    _mapType();
     return Container(child: Text('常用数据类型，请查看控制台输出'),);
   }
 
@@ -23,5 +27,58 @@ class _DataTypeState extends State<DataType> {
     int int1 = 3;
     double d1 = 1.68;
     print("int1:$int1, d1:$d1");
+
+    num num2 = -1.5;
+    print(num2.abs());
+    print(num2.toInt());
+    print(num2.toDouble());
+  }
+
+  void _stringType() {
+    String str1 = '字符串', str2 = "双引号";
+    String str3 = 'str1:$str1, str2:$str2';
+    String str4 = 'str1:' + str1 + ', str2:' + str2;
+    String str5 = '常用数据类型，请看控制台输出';
+    print(str3);
+    print(str4);
+    print(str5.substring(1, 5));
+    print(str5.indexOf('类型'));
+  }
+
+  _boolType() {
+    bool success = true, fail= false;
+    print(success || fail);
+    print(success && fail);
+  }
+
+  _listType() {
+    List list = [1, 2, 3, '集合'];
+    print(list);
+
+    List<int> list2 = [];
+    List list3 = [];
+    list3.add('list3');
+    list3.addAll(list);
+    print(list3);
+
+    List list4 = List.generate(3, (index) => index * 2);
+    print(list4);
+
+    for(int i = 0; i <list.length; i++)
+      print(list[i]);
+    print('--------------------------');
+    for(var o in list) {
+      print(o);
+    }
+    print('--------------------------');
+    list.forEach((element) {
+      print(element);
+    });
+    print('--------------------------');
+    list.forEach(print);
+  }
+
+  _mapType() {
+
   }
 }
